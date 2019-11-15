8669695ffe09707d91eae07d782f61c3b4167f62
yangjunjin <yangjunjin>
2019-11-15 12:48:59 +0800

    添加key

--- app/src/main/java/com/cq/mygaodemapdemo/MainActivity.java
+++ app/src/main/java/com/cq/mygaodemapdemo/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ///34343
     }
 
     public void onClick(View view) {
